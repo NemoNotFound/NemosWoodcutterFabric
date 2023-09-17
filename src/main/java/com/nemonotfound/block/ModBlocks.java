5d8f5eb9bb2ee46c8304136cf4565dcb30ef631d
NemoNotFound <02grimes-bard@icloud.com>
2023-09-17 19:17:59 +0200

    Add oak log sound to Woodcutter

--- src/main/java/com/nemonotfound/block/ModBlocks.java
+++ src/main/java/com/nemonotfound/block/ModBlocks.java
@@ -2,8 +2,10 @@ package com.nemonotfound.block;
 
 import net.minecraft.block.*;
 import net.minecraft.block.enums.Instrument;
+import net.minecraft.sound.BlockSoundGroup;
 
 public class ModBlocks {
 
-    public static final Block WOODCUTTER_BLOCK = new WoodcutterBlock(AbstractBlock.Settings.create().mapColor(MapColor.OAK_TAN).instrument(Instrument.BASEDRUM).strength(2.0f));
+    public static final Block WOODCUTTER_BLOCK = new WoodcutterBlock(
+            AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOD).mapColor(MapColor.OAK_TAN).instrument(Instrument.BASEDRUM).strength(2.0f));
 }
