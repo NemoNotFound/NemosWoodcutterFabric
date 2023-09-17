174e7e0a34f7981c974b95611baaee247e8ef9a3
NemoNotFound <02grimes-bard@icloud.com>
2023-09-17 19:37:48 +0200

    Refactoring/bug fix

--- src/main/java/com/nemonotfound/block/ModBlocks.java
+++ src/main/java/com/nemonotfound/block/ModBlocks.java
@@ -7,5 +7,9 @@ import net.minecraft.sound.BlockSoundGroup;
 public class ModBlocks {
 
     public static final Block WOODCUTTER_BLOCK = new WoodcutterBlock(
-            AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOD).mapColor(MapColor.OAK_TAN).instrument(Instrument.BASEDRUM).strength(2.0f));
+            AbstractBlock.Settings.create()
+                    .sounds(BlockSoundGroup.WOOD)
+                    .mapColor(MapColor.OAK_TAN)
+                    .instrument(Instrument.BASEDRUM)
+                    .strength(2.0f));
 }
