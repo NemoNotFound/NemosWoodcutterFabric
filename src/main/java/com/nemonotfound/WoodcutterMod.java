1693484cadfec2b267f54148af20c078d8ae45bd
NemoNotFound <02grimes-bard@icloud.com>
2023-10-15 15:53:42 +0200

    Added different ladder types

--- src/main/java/com/nemonotfound/WoodcutterMod.java
+++ src/main/java/com/nemonotfound/WoodcutterMod.java
@@ -30,10 +30,52 @@ public class WoodcutterMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		log.info("Thank you for using Nemo's Woodcutter!");
-		Registry.register(Registries.BLOCK, new Identifier(mod_id, "woodcutter"), ModBlocks.WOODCUTTER_BLOCK);
-		Registry.register(Registries.ITEM, new Identifier(mod_id, "woodcutter"), new BlockItem(ModBlocks.WOODCUTTER_BLOCK, new FabricItemSettings()));
+		registerBlocks();
+		registerItems();
+		addItemsToItemGroup();
 		WOODCUTTING = RecipeType.register("woodcutting");
 		WOODCUTTING_RECIPE_RECIPE_SERIALIZER = RecipeSerializer.register("woodcutting", new WoodcutterSerializer(WoodcuttingRecipe::new));
+	}
+
+	private void registerBlocks() {
+		Registry.register(Registries.BLOCK, new Identifier(mod_id, "woodcutter"), ModBlocks.WOODCUTTER_BLOCK);
+		Registry.register(Registries.BLOCK, new Identifier(mod_id, "dark_oak_ladder"), ModBlocks.DARK_OAK_LADDER);
+		Registry.register(Registries.BLOCK, new Identifier(mod_id, "acacia_ladder"), ModBlocks.ACACIA_LADDER);
+		Registry.register(Registries.BLOCK, new Identifier(mod_id, "birch_ladder"), ModBlocks.BIRCH_LADDER);
+		Registry.register(Registries.BLOCK, new Identifier(mod_id, "spruce_ladder"), ModBlocks.SPRUCE_LADDER);
+		Registry.register(Registries.BLOCK, new Identifier(mod_id, "warped_ladder"), ModBlocks.WARPED_LADDER);
+		Registry.register(Registries.BLOCK, new Identifier(mod_id, "bamboo_ladder"), ModBlocks.BAMBOO_LADDER);
+		Registry.register(Registries.BLOCK, new Identifier(mod_id, "crimson_ladder"), ModBlocks.CRIMSON_LADDER);
+		Registry.register(Registries.BLOCK, new Identifier(mod_id, "mangrove_ladder"), ModBlocks.MANGROVE_LADDER);
+		Registry.register(Registries.BLOCK, new Identifier(mod_id, "jungle_ladder"), ModBlocks.JUNGLE_LADDER);
+		Registry.register(Registries.BLOCK, new Identifier(mod_id, "bound_bamboo_ladder"), ModBlocks.BOUND_BAMBOO_LADDER);
+	}
+
+	private void registerItems() {
+		Registry.register(Registries.ITEM, new Identifier(mod_id, "woodcutter"), new BlockItem(ModBlocks.WOODCUTTER_BLOCK, new FabricItemSettings()));
+		Registry.register(Registries.ITEM, new Identifier(mod_id, "dark_oak_ladder"), new BlockItem(ModBlocks.DARK_OAK_LADDER, new FabricItemSettings()));
+		Registry.register(Registries.ITEM, new Identifier(mod_id, "acacia_ladder"), new BlockItem(ModBlocks.ACACIA_LADDER, new FabricItemSettings()));
+		Registry.register(Registries.ITEM, new Identifier(mod_id, "birch_ladder"), new BlockItem(ModBlocks.BIRCH_LADDER, new FabricItemSettings()));
+		Registry.register(Registries.ITEM, new Identifier(mod_id, "spruce_ladder"), new BlockItem(ModBlocks.SPRUCE_LADDER, new FabricItemSettings()));
+		Registry.register(Registries.ITEM, new Identifier(mod_id, "warped_ladder"), new BlockItem(ModBlocks.WARPED_LADDER, new FabricItemSettings()));
+		Registry.register(Registries.ITEM, new Identifier(mod_id, "bamboo_ladder"), new BlockItem(ModBlocks.BAMBOO_LADDER, new FabricItemSettings()));
+		Registry.register(Registries.ITEM, new Identifier(mod_id, "crimson_ladder"), new BlockItem(ModBlocks.CRIMSON_LADDER, new FabricItemSettings()));
+		Registry.register(Registries.ITEM, new Identifier(mod_id, "mangrove_ladder"), new BlockItem(ModBlocks.MANGROVE_LADDER, new FabricItemSettings()));
+		Registry.register(Registries.ITEM, new Identifier(mod_id, "jungle_ladder"), new BlockItem(ModBlocks.JUNGLE_LADDER, new FabricItemSettings()));
+		Registry.register(Registries.ITEM, new Identifier(mod_id, "bound_bamboo_ladder"), new BlockItem(ModBlocks.BOUND_BAMBOO_LADDER, new FabricItemSettings()));
+	}
+
+	private void addItemsToItemGroup() {
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> content.add(ModBlocks.WOODCUTTER_BLOCK));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> content.add(ModBlocks.DARK_OAK_LADDER));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> content.add(ModBlocks.ACACIA_LADDER));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> content.add(ModBlocks.BIRCH_LADDER));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> content.add(ModBlocks.SPRUCE_LADDER));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> content.add(ModBlocks.WARPED_LADDER));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> content.add(ModBlocks.BAMBOO_LADDER));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> content.add(ModBlocks.CRIMSON_LADDER));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> content.add(ModBlocks.MANGROVE_LADDER));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> content.add(ModBlocks.JUNGLE_LADDER));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> content.add(ModBlocks.BOUND_BAMBOO_LADDER));
 	}
 }
--- src/main/java/com/nemonotfound/block/ModBlocks.java
+++ src/main/java/com/nemonotfound/block/ModBlocks.java
@@ -4,12 +4,24 @@ import net.minecraft.block.*;
 import net.minecraft.block.enums.Instrument;
 import net.minecraft.sound.BlockSoundGroup;
 
+import static net.minecraft.block.Blocks.LADDER;
+
 public class ModBlocks {
 
-    public static final Block WOODCUTTER_BLOCK = new WoodcutterBlock(
-            AbstractBlock.Settings.create()
+    public static final Block WOODCUTTER_BLOCK = new WoodcutterBlock(AbstractBlock.Settings.create()
                     .sounds(BlockSoundGroup.WOOD)
                     .mapColor(MapColor.OAK_TAN)
                     .instrument(Instrument.BASS)
                     .strength(2.0f));
+    public static final Block DARK_OAK_LADDER = new LadderBlock(AbstractBlock.Settings.copy(LADDER));
+    public static final Block ACACIA_LADDER = new LadderBlock(AbstractBlock.Settings.copy(LADDER));
+    public static final Block BIRCH_LADDER = new LadderBlock(AbstractBlock.Settings.copy(LADDER));
+    public static final Block SPRUCE_LADDER = new LadderBlock(AbstractBlock.Settings.copy(LADDER));
+    public static final Block WARPED_LADDER = new LadderBlock(AbstractBlock.Settings.copy(LADDER));
+    public static final Block BAMBOO_LADDER = new LadderBlock(AbstractBlock.Settings.copy(LADDER));
+    public static final Block CRIMSON_LADDER = new LadderBlock(AbstractBlock.Settings.copy(LADDER));
+    public static final Block MANGROVE_LADDER = new LadderBlock(AbstractBlock.Settings.copy(LADDER));
+    public static final Block JUNGLE_LADDER = new LadderBlock(AbstractBlock.Settings.copy(LADDER));
+    public static final Block BOUND_BAMBOO_LADDER = new LadderBlock(AbstractBlock.Settings.copy(LADDER));
+
 }
