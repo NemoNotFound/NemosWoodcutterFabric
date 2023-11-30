abc69c1ad544bcbed21311c404d486ca7ca11712
NemoNotFound <02grimes-bard@icloud.com>
2023-11-30 21:31:57 +0100

    Adapt positioning of Woodcutter in group and update version to 1.8.1

--- src/main/java/com/nemonotfound/NemosWoodcutter.java
+++ src/main/java/com/nemonotfound/NemosWoodcutter.java
@@ -7,6 +7,7 @@ import com.nemonotfound.screen.WoodcutterScreenHandler;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.recipe.RecipeSerializer;
@@ -34,6 +35,7 @@ public class NemosWoodcutter implements ModInitializer {
 		Registry.register(Registries.ITEM, new Identifier(MOD_ID, "woodcutter"), new BlockItem(ModBlocks.WOODCUTTER_BLOCK, new FabricItemSettings()));
 		WOODCUTTING = RecipeType.register("woodcutting");
 		WOODCUTTING_RECIPE_RECIPE_SERIALIZER = RecipeSerializer.register("woodcutting", new WoodcutterSerializer(WoodcuttingRecipe::new));
-		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> content.add(ModBlocks.WOODCUTTER_BLOCK));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> content.addAfter(Blocks.STONECUTTER,
+				ModBlocks.WOODCUTTER_BLOCK));
 	}
 }
